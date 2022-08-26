141ab66d953cf70f54b6c7db9c93c776a6bf9764
EC2 Default User <ec2-user@ip-172-31-13-253.ap-northeast-1.compute.internal>
2022-08-26 11:09:22 +0000

    commit

---
+++ app/controllers/public/customers_controller.rb
@@ -0,0 +1,17 @@
+class Public::CustomersController < ApplicationController
+  def show
+    @customer = current_user
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def confirmation
+  end
+
+  def withdrawal
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
+
+  namespace :public do
+    get 'customers/show'
+    get 'customers/edit'
+    get 'customers/update'
+    get 'customers/confirmation'
+    get 'customers/withdrawal'
+  end
+  #顧客用
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  #管理者用
+  devise_for :admin,skip: [:registrations, :passwords], controllers: {
+    sessions: 'admin/sessions'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
